ca4757650fc0d8fa92650c7fdb96a381c1feb5c2
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-02-15 10:30:24 +0500

    User Joshua Sullivan saved changes to main.py at 02/15/2026, 05:30:23 UTC time.

--- 61fd0d2d-7616-4cbd-a3aa-642a6ae79a51/main.py
+++ 61fd0d2d-7616-4cbd-a3aa-642a6ae79a51/main.py
@@ -1,23 +1,252 @@
-from surmount.base_class import Strategy, TargetAllocation, backtest
-from surmount.logging import log
+> starting backtest...
 
-class TradingStrategy(Strategy):
-    def __init__(self):
-        self.tickers = ["AAPL", "TSLA", "SPY"]
-        self.weights = [30, 30, 30]
-        self.count = 0
+> fetching backtest logs...
 
-    @property
-    def interval(self):
-        return "1day"
-
-    @property
-    def assets(self):
-        return self.tickers
-
-    def run(self, data):
-        self.count += 1
-        if (self.count % 30 == 1):
-            allocation_dict = {self.tickers[i]: self.weights[i]/sum(self.weights) for i in range(len(self.tickers))}
-            return TargetAllocation(allocation_dict)
-        return None
+> [INFO] Starting backtest
+> - Start date: 2025-02-14 00:00:00
+> - End date: 2026-02-14 00:00:00
+> - Initial capital: 100000.0
+> - Slippage: 0.0
+> - Fees: 0.0
+> [INFO] Pulling source code
+> [INFO] Source code cloned, running on historical data
+> [LOG] [2025-02-13 15:55:00] NITRO SEED K: Live. Targets: ['SOXL', 'FNGU', 'DFEN', 'UCO', 'URNM', 'BITU', 'SILJ']
+> [LOG] [2025-02-13 15:55:00] ENTRY: FNGU at 681.55
+> [LOG] [2025-02-18 11:40:00] EXIT: FNGU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-02-18 15:00:00] ENTRY: SOXL at 30.04
+> [LOG] [2025-02-20 12:45:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-02-26 09:55:00] ENTRY: DFEN at 30.77
+> [LOG] [2025-02-26 13:00:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-02-27 10:10:00] ENTRY: DFEN at 31.16
+> [LOG] [2025-03-03 13:10:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-03-05 15:00:00] ENTRY: SOXL at 21.99
+> [LOG] [2025-03-06 09:30:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-03-07 15:50:00] ENTRY: SOXL at 20.94
+> [LOG] [2025-03-10 09:30:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-03-12 13:25:00] ENTRY: DFEN at 28.485
+> [LOG] [2025-03-13 13:30:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-03-14 10:30:00] ENTRY: SOXL at 19.19
+> [LOG] [2025-03-18 11:05:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-03-19 10:20:00] ENTRY: BITU at 35.15
+> [LOG] [2025-03-20 12:25:00] EXIT: BITU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-03-20 15:45:00] ENTRY: BITU at 35.23
+> [LOG] [2025-03-21 09:35:00] EXIT: BITU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-03-21 13:40:00] ENTRY: SOXL at 19.32
+> [LOG] [2025-03-24 14:10:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-03-25 11:00:00] ENTRY: FNGU at 17.02
+> [LOG] [2025-03-25 13:55:00] EXIT: FNGU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-03-26 10:50:00] ENTRY: UCO at 26.39
+> [LOG] [2025-03-26 13:45:00] EXIT: UCO Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-04-01 10:50:00] ENTRY: FNGU at 13.44
+> [LOG] [2025-04-01 14:20:00] EXIT: FNGU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-04-02 11:10:00] ENTRY: FNGU at 13.79
+> [LOG] [2025-04-02 14:25:00] EXIT: FNGU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-04-08 09:55:00] ENTRY: DFEN at 23.5
+> [LOG] [2025-04-08 11:15:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-04-09 09:30:00] ENTRY: SILJ at 10.75
+> [LOG] [2025-04-09 10:55:00] EXIT: SILJ Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-04-09 14:15:00] ENTRY: SOXL at 12.18
+> [LOG] [2025-04-10 09:50:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-04-10 13:10:00] ENTRY: SILJ at 11.48
+> [LOG] [2025-04-15 12:55:00] EXIT: SILJ Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-04-22 15:45:00] ENTRY: BITU at 39.68
+> [LOG] [2025-04-23 15:25:00] EXIT: BITU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-04-24 12:15:00] ENTRY: SOXL at 11.76
+> [LOG] [2025-04-28 12:10:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-04-28 15:30:00] ENTRY: SOXL at 12.185
+> [LOG] [2025-04-29 12:45:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-04-30 12:45:00] ENTRY: DFEN at 30.01
+> [LOG] [2025-05-01 12:20:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-05-01 15:40:00] ENTRY: UCO at 19.49
+> [LOG] [2025-05-02 09:45:00] EXIT: UCO Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-05-02 13:05:00] ENTRY: SOXL at 13.49
+> [LOG] [2025-05-05 11:30:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-05-05 14:50:00] ENTRY: BITU at 42.15
+> [LOG] [2025-05-06 09:30:00] EXIT: BITU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-05-07 15:55:00] ENTRY: SOXL at 13.27
+> [LOG] [2025-05-14 13:20:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-05-15 11:00:00] ENTRY: DFEN at 38.04
+> [LOG] [2025-05-20 14:50:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-05-27 09:30:00] ENTRY: SOXL at 16.48
+> [LOG] [2025-05-29 13:15:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-05-30 10:35:00] ENTRY: DFEN at 43.11
+> [LOG] [2025-05-30 11:15:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-05-30 15:15:00] ENTRY: DFEN at 43.51
+> [LOG] [2025-06-03 11:00:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-06-03 14:20:00] ENTRY: DFEN at 44.85
+> [LOG] [2025-06-04 15:05:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-06-05 11:55:00] ENTRY: SILJ at 15.1
+> [LOG] [2025-06-06 12:30:00] EXIT: SILJ Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-06-06 15:50:00] ENTRY: URNM at 41.95
+> [LOG] [2025-06-09 15:30:00] EXIT: URNM Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-06-10 12:20:00] ENTRY: SOXL at 21.77
+> [LOG] [2025-06-11 13:50:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-06-12 10:40:00] ENTRY: SOXL at 22.05
+> [LOG] [2025-06-12 14:10:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-06-16 09:30:00] ENTRY: URNM at 47.42
+> [LOG] [2025-06-16 11:20:00] EXIT: URNM Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-06-16 15:55:00] ENTRY: BITU at 53.27
+> [LOG] [2025-06-17 09:30:00] EXIT: BITU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-06-17 13:10:00] ENTRY: DFEN at 45.65
+> [LOG] [2025-06-18 12:00:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-06-23 14:50:00] ENTRY: BITU at 47.66
+> [LOG] [2025-06-25 13:40:00] EXIT: BITU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-06-26 10:30:00] ENTRY: DFEN at 49.26
+> [LOG] [2025-06-26 11:50:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-06-26 15:10:00] ENTRY: FNGU at 23.765
+> [LOG] [2025-06-27 14:50:00] EXIT: FNGU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-06-30 11:40:00] ENTRY: FNGU at 24.7599
+> [LOG] [2025-07-01 10:30:00] EXIT: FNGU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-07-01 13:50:00] ENTRY: SOXL at 25.03
+> [LOG] [2025-07-03 14:00:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-07-07 10:50:00] ENTRY: DFEN at 51.32
+> [LOG] [2025-07-07 12:15:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-07-09 09:30:00] ENTRY: BITU at 53.13
+> [LOG] [2025-07-14 11:45:00] EXIT: BITU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-07-14 15:55:00] ENTRY: URNM at 46.94
+> [LOG] [2025-07-16 15:10:00] EXIT: URNM Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-07-17 12:00:00] ENTRY: SOXL at 27.44
+> [LOG] [2025-07-21 15:05:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-07-22 12:00:00] ENTRY: BITU at 62.27
+> [LOG] [2025-07-23 09:40:00] EXIT: BITU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-07-23 13:00:00] ENTRY: SOXL at 25.67
+> [LOG] [2025-07-25 09:30:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-07-25 12:50:00] ENTRY: SOXL at 25.54
+> [LOG] [2025-07-29 15:35:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-07-30 13:40:00] ENTRY: UCO at 26.78
+> [LOG] [2025-07-30 14:05:00] EXIT: UCO Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-07-31 10:55:00] ENTRY: FNGU at 26.05
+> [LOG] [2025-07-31 15:20:00] EXIT: FNGU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-08-04 15:55:00] ENTRY: URNM at 46.47
+> [LOG] [2025-08-06 14:50:00] EXIT: URNM Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-08-07 11:40:00] ENTRY: SOXL at 25.09
+> [LOG] [2025-08-11 13:25:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-08-12 10:15:00] ENTRY: SOXL at 26.82
+> [LOG] [2025-08-14 14:10:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-08-15 11:00:00] ENTRY: SILJ at 16.96
+> [LOG] [2025-08-15 14:55:00] EXIT: SILJ Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-08-18 12:05:00] ENTRY: URNM at 47.5
+> [LOG] [2025-08-19 10:10:00] EXIT: URNM Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-08-22 09:35:00] ENTRY: URNM at 46.75
+> [LOG] [2025-08-25 12:45:00] EXIT: URNM Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-08-26 09:35:00] ENTRY: URNM at 50.25
+> [LOG] [2025-08-26 12:30:00] EXIT: URNM Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-08-26 15:55:00] ENTRY: BITU at 52.03
+> [LOG] [2025-08-28 15:55:00] EXIT: BITU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-09-03 10:45:00] ENTRY: FNGU at 25.795
+> [LOG] [2025-09-03 14:20:00] EXIT: FNGU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-09-04 11:10:00] ENTRY: SOXL at 24.91
+> [LOG] [2025-09-08 14:00:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-09-09 10:50:00] ENTRY: UCO at 23.15
+> [LOG] [2025-09-09 12:00:00] EXIT: UCO Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-09-09 15:20:00] ENTRY: DFEN at 56.72
+> [LOG] [2025-09-11 14:10:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-09-12 11:00:00] ENTRY: UCO at 23.14
+> [LOG] [2025-09-12 12:30:00] EXIT: UCO Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-09-12 15:50:00] ENTRY: BITU at 56.4
+> [LOG] [2025-09-15 09:30:00] EXIT: BITU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-09-15 12:50:00] ENTRY: FNGU at 29.7667
+> [LOG] [2025-09-17 10:40:00] EXIT: FNGU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-09-17 14:00:00] ENTRY: SOXL at 31.23
+> [LOG] [2025-09-17 14:10:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-09-18 11:00:00] ENTRY: SOXL at 33.54
+> [LOG] [2025-09-19 10:40:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-09-19 14:00:00] ENTRY: URNM at 58.38
+> [LOG] [2025-09-22 09:30:00] EXIT: URNM Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-09-22 12:50:00] ENTRY: URNM at 59.55
+> [LOG] [2025-09-23 13:35:00] EXIT: URNM Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-09-26 12:40:00] ENTRY: SILJ at 22.52
+> [LOG] [2025-09-29 14:55:00] EXIT: SILJ Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-09-30 11:45:00] ENTRY: DFEN at 67.34
+> [LOG] [2025-10-02 11:00:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-10-02 14:20:00] ENTRY: BITU at 58.61
+> [LOG] [2025-10-07 10:45:00] EXIT: BITU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-10-07 15:50:00] ENTRY: UCO at 22.22
+> [LOG] [2025-10-08 14:45:00] EXIT: UCO Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-10-09 11:35:00] ENTRY: URNM at 62.1
+> [LOG] [2025-10-09 12:55:00] EXIT: URNM Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-10-10 09:45:00] ENTRY: URNM at 63.53
+> [LOG] [2025-10-10 13:50:00] EXIT: URNM Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-10-14 11:50:00] ENTRY: URNM at 66.01
+> [LOG] [2025-10-14 15:45:00] EXIT: URNM Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-10-15 12:35:00] ENTRY: SOXL at 39.53
+> [LOG] [2025-10-15 13:10:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-10-16 10:00:00] ENTRY: SOXL at 40.96
+> [LOG] [2025-10-17 11:20:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-10-17 15:20:00] ENTRY: SOXL at 40.46
+> [LOG] [2025-10-21 09:30:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-10-21 12:50:00] ENTRY: BITU at 50.97
+> [LOG] [2025-10-21 14:35:00] EXIT: BITU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-10-23 12:55:00] ENTRY: SOXL at 41.08
+> [LOG] [2025-10-30 12:30:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-11-03 09:30:00] ENTRY: SOXL at 49.5
+> [LOG] [2025-11-03 13:50:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-11-05 15:55:00] ENTRY: SILJ at 20.76
+> [LOG] [2025-11-06 12:10:00] EXIT: SILJ Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-11-10 09:30:00] ENTRY: SOXL at 45.3
+> [LOG] [2025-11-10 11:00:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-11-10 14:20:00] ENTRY: DFEN at 69.025
+> [LOG] [2025-11-11 09:30:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-11-11 12:50:00] ENTRY: DFEN at 68.7
+> [LOG] [2025-11-11 15:30:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-11-12 12:20:00] ENTRY: SILJ at 23.95
+> [LOG] [2025-11-13 09:40:00] EXIT: SILJ Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-11-19 15:15:00] ENTRY: SOXL at 36.22
+> [LOG] [2025-11-20 11:55:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-11-21 12:05:00] ENTRY: SOXL at 32.03
+> [LOG] [2025-11-25 09:40:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-11-25 13:00:00] ENTRY: SOXL at 35.18
+> [LOG] [2025-11-28 14:25:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-12-01 11:20:00] ENTRY: SILJ at 26.28
+> [LOG] [2025-12-02 09:35:00] EXIT: SILJ Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-12-02 15:55:00] ENTRY: SILJ at 26.03
+> [LOG] [2025-12-03 13:05:00] EXIT: SILJ Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-12-04 09:55:00] ENTRY: DFEN at 60.47
+> [LOG] [2025-12-05 11:00:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-12-05 14:20:00] ENTRY: SOXL at 47.02
+> [LOG] [2025-12-11 09:50:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-12-11 13:10:00] ENTRY: DFEN at 59.23
+> [LOG] [2025-12-12 12:35:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-12-15 09:45:00] ENTRY: SILJ at 27.72
+> [LOG] [2025-12-15 13:20:00] EXIT: SILJ Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-12-18 10:20:00] ENTRY: SOXL at 38.94
+> [LOG] [2025-12-24 15:15:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2025-12-26 12:05:00] ENTRY: SILJ at 29.82
+> [LOG] [2025-12-29 09:30:00] EXIT: SILJ Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-01-05 09:30:00] ENTRY: SOXL at 50.86
+> [LOG] [2026-01-05 12:30:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-01-05 15:50:00] ENTRY: BITU at 25.49
+> [LOG] [2026-01-06 11:25:00] EXIT: BITU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-01-06 14:45:00] ENTRY: DFEN at 78.17
+> [LOG] [2026-01-07 12:50:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-01-08 10:55:00] ENTRY: UCO at 19
+> [LOG] [2026-01-09 13:05:00] EXIT: UCO Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-01-12 09:55:00] ENTRY: SILJ at 32.21
+> [LOG] [2026-01-13 12:50:00] EXIT: SILJ Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-01-15 10:00:00] ENTRY: SOXL at 60.71
+> [LOG] [2026-01-15 15:30:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-01-16 12:20:00] ENTRY: DFEN at 92.7
+> [LOG] [2026-01-20 10:50:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-01-21 15:55:00] ENTRY: BITU at 22.95
+> [LOG] [2026-01-22 11:05:00] EXIT: BITU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-01-22 14:25:00] ENTRY: DFEN at 85.37
+> [LOG] [2026-01-22 15:00:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-01-23 11:50:00] ENTRY: UCO at 21.6
+> [LOG] [2026-01-23 12:15:00] EXIT: UCO Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-01-23 15:35:00] ENTRY: SILJ at 38.75
+> [LOG] [2026-01-26 14:50:00] EXIT: SILJ Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-01-27 11:40:00] ENTRY: SOXL at 65.73
+> [LOG] [2026-01-27 15:50:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-01-28 12:40:00] ENTRY: SOXL at 69.55
+> [LOG] [2026-01-29 10:50:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-02-02 09:50:00] ENTRY: FNGU at 22.83
+> [LOG] [2026-02-02 14:40:00] EXIT: FNGU Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-02-03 11:50:00] ENTRY: SILJ at 33.47
+> [LOG] [2026-02-03 13:20:00] EXIT: SILJ Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-02-06 11:00:00] ENTRY: SOXL at 59.83
+> [LOG] [2026-02-10 15:10:00] EXIT: SOXL Trend Broken. Lockdown 3.5 hrs.
+> [LOG] [2026-02-12 09:30:00] ENTRY: DFEN at 81.65
+> [LOG] [2026-02-12 11:20:00] EXIT: DFEN Trend Broken. Lockdown 3.5 hrs.
+> [INFO] Backtest completed successfully!
+> [INFO] Storing backtest results
+> Backtesting is completed
